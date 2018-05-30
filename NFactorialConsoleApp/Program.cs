0a5758d0877d0205ce61e3c405d79a88c3714f71
STRUCTUREIT\kuzivakwashe.muvezwa <kuzivakwashe.muvezwa@structureit.net>
2018-05-30 13:59:51 +0200

    Adding Completed solution for N - Factorial (N!)

---
+++ NFactorialConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NFactorialConsoleApp.Services;
+
+namespace NFactorialConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculate = new Calculate();
+            Console.WriteLine("Enter a number N and we will return N!");
+            var nString  = Console.ReadLine();
+            Console.WriteLine(nString + "! =" + calculate.Factorial(Convert.ToInt32(nString)));
+        }
+    }
+}
---
+++ NFactorialConsoleApp/Services/ICalculate.cs
@@ -0,0 +1,20 @@
+﻿namespace NFactorialConsoleApp.Services
+{
+    public interface ICalculate
+    {
+        string Factorial(int n);
+    }
+
+    public class Calculate
+    {
+        public string nFactorial = " ";
+
+        public string Factorial(int n = 1, string delimiter = " x ")
+        {
+            if (n <= 1) return nFactorial + "1";
+            nFactorial = nFactorial + n + delimiter;
+            Factorial(--n, delimiter);
+            return nFactorial + "1";
+        }
+    }
+}
